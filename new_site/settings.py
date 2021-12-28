9908eaa668fff2f982c5648262be8a9ca52dfd9b
bakhrullo <81313486+bakhrullo@users.noreply.github.com>
2021-12-28 18:06:58 +0300

    Update settings.py

--- new_site/settings.py
+++ new_site/settings.py
@@ -24,9 +24,44 @@ SECRET_KEY = 'django-insecure-v91tyqp_bf#cxywmxr5ze32@%@$r=rvo@+_3m%raznye*eyk7n
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+DEBUG_PROPAGATE_EXCEPTIONS = True
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'formatters': {
+      'verbose': {
+         'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+            'pathname=%(pathname)s lineno=%(lineno)s ' +
+            'funcname=%(funcName)s %(message)s'),
+         'datefmt': '%Y-%m-%d %H:%M:%S'
+      },
+      'simple': {
+         'format': '%(levelname)s %(message)s'
+      }
+   },
+   'handlers': {
+      'null': {
+         'level': 'DEBUG',
+         'class': 'logging.NullHandler',
+      },
+      'console': {
+         'level': 'DEBUG',
+         'class': 'logging.StreamHandler',
+         'formatter': 'verbose'
+      }
+   },
+   'loggers': {
+      'testlogger': {
+         'handlers': ['console'],
+         'level': 'INFO',
+      }
+   }
+}
 
 ALLOWED_HOSTS = ['127.0.0.1', 'nazarovapp.herokuapp.com']
+
+
+
 
 
 # Application definition
